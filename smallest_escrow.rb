fee823a4ddde8256dbf642edc251465af690babb
Don Park <don@donpark.org>
2011-08-08 11:41:13 -0700

    cleanups

--- smallest_escrow.rb
+++ smallest_escrow.rb
@@ -1,6 +1,12 @@
 require 'sinatra/base'
-require 'digest/sha2'
 
 class SmallestEscrow < Sinatra::Base
+
+  set :static, true
+  set :show_execptions, true
+  
+  get '/' do
+    erb :create
+  end
 end
 
