cd5fd7e083560a9f753ab9ce60384eca23c59c2e
Don Park <don@donpark.org>
2011-08-08 13:51:12 -0700

    save and load form data

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source :rubygems
+
+gem 'sinatra'
+gem 'uuid'
+gem 'redis'
+
--- smallest_escrow.rb
+++ smallest_escrow.rb
@@ -1,4 +1,6 @@
 require 'sinatra/base'
+require 'uuid'
+require 'redis'
 
 class SmallestEscrow < Sinatra::Base
 
@@ -9,5 +11,29 @@ class SmallestEscrow < Sinatra::Base
   get '/' do
     erb :create
   end
+
+  get '/:uuid' do
+    uuid = params[:uuid]
+    offer = redis.get("deal:#{uuid}")
+    log("load deal:#{uuid} -> #{offer}")
+    erb :show, :locals => {:offer => offer}
+  end
+
+  post '/create' do
+    uuid = UUID.generate
+    redis.set("deal:#{uuid}", params)
+    log("save deal:#{uuid} -> #{params}")
+    redirect to("/#{uuid}")
+  end
+
+  private
+  def redis
+    @redis ||= Redis.new
+  end
+
+  def log(msg)
+    File.open("log","a"){|f| f.write msg+"\n"}
+  end
+
 end
 
