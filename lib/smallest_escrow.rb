a1d0f68e03bfbb23c7af4cd1a95da0041ae27e90
Don Park <don@donpark.org>
2011-08-23 14:09:47 -0700

    start bitbank

--- lib/smallest_escrow.rb
+++ lib/smallest_escrow.rb
@@ -1,8 +1,12 @@
 require 'dm-core'
 require 'dm-redis-adapter'
 require 'redis'
+require 'bitbank'
 
 require 'smallest_escrow/deal'
 
 # Datamapper
 DataMapper.setup(:default, {:adapter  => "redis", :host => "localhost", :port => 6380})
+
+# bitcoind
+BITBANK = Bitbank.new('bitbank.yml')
