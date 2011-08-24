07e3972805603bed7dd74a71ce8cff6d53e09c90
Don Park <don@donpark.org>
2011-08-24 08:24:36 -0700

    dwolla.rb

---
+++ lib/smallest_escrow/dwolla.rb
@@ -0,0 +1,15 @@
+class Dwolla
+  def initialize(yml_config)
+    @config = YAML.load(File.open(yml_config))
+    @api = Savon::Client.new { wsdl.document = "https://www.dwolla.com/api/API.svc?wsdl"}
+  end
+
+  def request_payment_key(amount, deal)
+    result = DWOLLA_API.request :wsdl, :request_payment_key, 
+                                       { :ApiKey => @config['key'], 
+                                         :ApiCode => @config['secret'], 
+                                         :Amount => amount, 
+                                         :Description => "SE:#{deal.uuid}", 
+                                         :CustomerId => deal.dwolla_receiving_address }
+  end
+end
