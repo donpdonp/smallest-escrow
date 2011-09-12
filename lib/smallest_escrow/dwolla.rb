cfa2067ab2c98a959c4b2340554df2cac8d90229
Don Park <don@donpark.org>
2011-09-12 15:06:49 -0700

    the mysterious soap legacy access credentials

--- lib/smallest_escrow/dwolla.rb
+++ lib/smallest_escrow/dwolla.rb
@@ -44,8 +44,8 @@ class Dwolla
   # SOAP API
   def request_payment_key(deal)
     result = @soap.request :wsdl, :request_payment_key, 
-                                 { :ApiKey => @config['key'], 
-                                   :ApiCode => @config['secret'], 
+                                 { :ApiKey => @config['soap_key'], 
+                                   :ApiCode => @config['soap_secret'], 
                                    :Amount => deal.usd, 
                                    :Description => "SE:#{deal.uuid}", 
                                    :CustomerId => deal.dwolla_receiving_address }
