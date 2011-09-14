d34e7392cf1d8f06928ccf82e0ee6c693c6162fa
Don Park <don@donpark.org>
2011-09-14 12:33:39 -0700

    OrderId is int only and does not show up in transaction log.

--- lib/smallest_escrow/dwolla/api.rb
+++ lib/smallest_escrow/dwolla/api.rb
@@ -21,7 +21,6 @@ module SmallestEscrow
       def request(deal)
         data = { :Key => @config['key'],
                  :Secret => @config['secret'],
-                 :OrderId => deal.uuid.gsub('-',''),
                  :Callback => @config['host']+"/dwolla/payment",
                  :Redirect => @config['host']+"/#{deal.uuid}",
                  :PurchaseOrder => {
